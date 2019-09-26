dd5d28ef4320bef214766ca5f27d49176c3f2bf8
JKhandelwal <jk218@st-andrews.ac.uk>
2019-09-26 22:14:37 +0100

    merging done

--- r_code.r
+++ r_code.r
@@ -11,7 +11,6 @@ read_clean_data <- function(name){
   return(filt_data)
 }
 
-
 get_winner <- function(df){
   # Filter the winner_ID
   # https://stackoverflow.com/questions/7381455/filtering-a-data-frame-by-values-in-a-column
@@ -19,7 +18,6 @@ get_winner <- function(df){
   winner_ID <- unique(winner["selectionId"])$selectionId
   return(winner_ID)
 }
-
 
 pick_three <- function(winner_ID, df){
   runners <- df[df$selectionId != toString(winner_ID),]
@@ -32,7 +30,6 @@ pick_three <- function(winner_ID, df){
 filter_to_three <- function(df, vector){
   return(df[df$selectionId %in% vector,])
 }
-
 
 plot_odds <- function(df){
   # Give the chart file a name.
@@ -61,48 +58,75 @@ mean_variance <- function(df){
   mean_back <- mean(df$bckPrc1)
   var_lay <- var(df$laySz1)
   var_back <- var(df$bckSz1)
-  print(c(mean_lay, mean_back, var_lay, var_back))
   return(c(mean_lay, mean_back, var_lay, var_back))
 }
 
 plot_distribution <- function(df){
-  png(file = paste("Backing Distribution for time period up to 5 minutes before", toString(unique(df$selectionId)),".jpg"))
-  hist(df$bckPrc1)
+  png(file = paste("Backing Distribution from 5 minutes before to start", toString(unique(df$selectionId)),".jpg"))
+  hist(df$bckPrc1, main = paste("Backing Distribution from 5 minutes before to start", toString(unique(df$selectionId))))
   dev.off()
 }
 
-filtered_data <- read_clean_data("Ascot 4pm 20th June 2018 1 mile handicap.xlsx")
+build_and_merge <-function(runners, winner_id, all_runner_data){
+  df <- setNames(data.frame(matrix(ncol = 2, nrow = 0)), c("selectionId", "Win/Loss"))
+  for (i in (1:4)){
+    if (runners[i] == winner_ID){
+      df[nrow(df) + 1,] <- c(runners[i], "WINNER")
+    } else {
+      df[nrow(df) + 1,] <- c(runners[i], "LOSER")
+    }
+  }
+  merged <- merge(all_runner_data, df,by="selectionId")
+  return(merged)
+}
 
+numerify <- function(filt_runner_data){
+  # Ask about Type Coercion
+  filt_runner_data$layPrc1 <- as.numeric(filt_runner_data$layPrc1)
+  filt_runner_data$laySz1 <- as.numeric(filt_runner_data$laySz1)
+  filt_runner_data$bckPrc1 <- as.numeric(filt_runner_data$bckPrc1)
+  filt_runner_data$bckSz1 <- as.numeric(filt_runner_data$bckSz1)
+
+  return(filt_runner_data)
+}
+
+filtered_data <- read_clean_data("Ascot 4pm 20th June 2018 1 mile handicap.xlsx")
 start_time <- as.numeric(filtered_data[min(which(filtered_data$inplay == TRUE)), "callTime_GMT"])
 
 winner_ID <- get_winner(filtered_data)
 three_runners <- pick_three(winner_ID, filtered_data)
-all_runners <- c(winner_ID, three_runners)
-runners_data <- filter_to_three(filtered_data, all_runners)
-# Ask about Type Coercion
-runners_data$layPrc1 <- as.numeric(runners_data$layPrc1)
-runners_data$laySz1 <- as.numeric(runners_data$laySz1)
-runners_data$bckPrc1 <- as.numeric(runners_data$bckPrc1)
-runners_data$bckSz1 <- as.numeric(runners_data$bckSz1)
+
+filt_runner_list <- c(winner_ID, three_runners)
+filt_runner_data <- filter_to_three(filtered_data, filt_runner_list)
+filt_runner_data <- numerify(filt_runner_data)
 
 for (i in (1:4)){
   print("--------------------")
-  print(paste("Competitor ", toString(all_runners[i])))
+  print(paste("Competitor ", toString(filt_runner_list[i])))
   print("--------------------")
-  # !±!!!!!!!!!!!!!!!!!!!!!!! UNCOMMENT
-  # plot_odds(filter(runners_data, selectionId == all_runners[i] & inplay == "FALSE"))
-  # plot_volumes(filter(runners_data, selectionId == all_runners[i] & inplay == "FALSE"))
+  # !±!!!!!!!!!!!!!!!!!!!!!!! TODO UNCOMMENT ALL PLOTS
+  # plot_odds(filter(filt_runner_data, selectionId == filt_runner_list[i] & inplay == "FALSE"))
+  # plot_volumes(filter(filt_runner_data, selectionId == filt_runner_list[i] & inplay == "FALSE"))
 
-  up_to_fifteen = mean_variance(filter(runners_data, marketStatus != "SUSPENDED" & selectionId == all_runners[i] & callTime_GMT < (start_time - 15*60)))
-  fifteen_to_five = mean_variance(filter(runners_data, marketStatus != "SUSPENDED" &  selectionId == all_runners[i] & callTime_GMT >= (start_time - 15*60) & callTime_GMT < (start_time - 5*60)))
-  five_to_start = mean_variance(filter(runners_data, marketStatus != "SUSPENDED" &  selectionId == all_runners[i] & callTime_GMT >= (start_time - 5*60) & callTime_GMT < start_time ))
+  up_to_fifteen <- mean_variance(filter(filt_runner_data, marketStatus != "SUSPENDED" & selectionId == filt_runner_list[i] & callTime_GMT < (start_time - 15*60)))
+  fifteen_to_five <- mean_variance(filter(filt_runner_data, marketStatus != "SUSPENDED" &  selectionId == filt_runner_list[i] & callTime_GMT >= (start_time - 15*60) & callTime_GMT < (start_time - 5*60)))
+  five_to_start <- mean_variance(filter(filt_runner_data, marketStatus != "SUSPENDED" &  selectionId == filt_runner_list[i] & callTime_GMT >= (start_time - 5*60) & callTime_GMT < start_time ))
 
   mean_variance_df <- setNames(data.frame(matrix(ncol = 4, nrow = 0)), c("Mean_Lay_Odds", "Mean_Backing_odds", "Variance_Lay_Odds", "Variance_Backing_Odds"))
-  mean_variance_df[nrow(mean_variance_df) + 1,] = up_to_fifteen
-  mean_variance_df[nrow(mean_variance_df) + 1,] = fifteen_to_five
-  mean_variance_df[nrow(mean_variance_df) + 1,] = five_to_start
+  mean_variance_df[nrow(mean_variance_df) + 1,] <- up_to_fifteen
+  mean_variance_df[nrow(mean_variance_df) + 1,] <- fifteen_to_five
+  mean_variance_df[nrow(mean_variance_df) + 1,] <- five_to_start
 
-  print(mean_variance_df)
+  # Print out the Mean_Variance
+  # TODO UNCOMMENT !!!
+  # print(mean_variance_df)
 
-  plot_distribution(filter(runners_data, marketStatus != "SUSPENDED" &  selectionId == all_runners[i] & callTime_GMT >= (start_time - 5*60) & callTime_GMT < start_time))
+  # TODO Uncomment!!!!!!!!!
+  # plot_distribution(filter(filt_runner_data, marketStatus != "SUSPENDED" &  selectionId == filt_runner_list[i] & callTime_GMT >= (start_time - 5*60) & callTime_GMT < start_time))
 }
+
+merged <- build_and_merge(filt_runner_list, winner_ID, filt_runner_data)
+print(unique(merged$selectionId))
+print("===============")
+print(head(filter(merged, selectionId == winner_ID)))
+print("===============")
